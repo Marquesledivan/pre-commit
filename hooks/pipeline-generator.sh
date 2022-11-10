8c24a10201047b7d9a94b05d3b2eb28f0795572f
Ledivan Bernardo Marques <ledivan_bernardo@yahoo.com.br>
2022-11-10 12:58:09 -0300

    Update pipeline-generator.sh

--- hooks/pipeline-generator.sh
+++ hooks/pipeline-generator.sh
@@ -15,18 +15,20 @@ if [ -z "$PGEN_EXTRA_KNOW_HOST" ]; then echo "set \$PGEN_EXTRA_KNOW_HOST variabl
 if [ -z "$PGEN_IMAGE" ]; then echo "set \$PGEN_IMAGE variable." && exit 1; fi
 if [ -z "$PGEN_BRANCH" ]; then echo "set \$PGEN_BRANCH variable." && exit 1; fi
 if [ -z "$PGEN_OUTPUT_FILE" ]; then echo "set \$PGEN_OUTPUT_FILE variable." && exit 1; fi
+if [ -z "$ENABLE_ASDF" ]; then echo "set \$ENABLE_ASDF variable." && exit 1; fi
+
 
 # Install pipeline generator.
 if ! command -v pipeline-generator &> /dev/null
 then
     echo "installing pipeline-generator"
-    pip install -U git+https://github.com/Marquesledivan/pipeline-generator.git@v0.0.5
+    pip install -U git+https://github.com/Marquesledivan/pipeline-generator.git@v0.0.7
 fi
 
 # Clean cache.
 find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;
 
-pipeline-generator -i $PGEN_IMAGE -e $PGEN_EXTRA_KNOW_HOST -o $PGEN_OUTPUT_FILE -b $PGEN_BRANCH
+pipeline-generator -i $PGEN_IMAGE -e $PGEN_EXTRA_KNOW_HOST -o $PGEN_OUTPUT_FILE -b $PGEN_BRANCH -a $ENABLE_ASDF
 
 # Add to commit.
 git add .
