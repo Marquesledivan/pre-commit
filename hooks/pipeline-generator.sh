2f0f27f47f939b0c38d448d36e736cac19bbbd9c
Ledivan Bernardo <ledivan.bernardo@ripio.com>
2022-11-09 22:00:03 -0300

    first commit

---
+++ hooks/pipeline-generator.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Run pre-commits-hooks
+# Install these tools.
+# brew install pre-commit gawk terraform-docs tflint tfsec coreutils checkov terrascan
+
+set -e 
+
+# Load variables defined in .pgen-env
+if [ -z ".pgen-env" ]; then echo "Create and define vars in .pgen-env fiel." && exit 1; fi
+. .pgen-env
+
+# Check if variables are loaded.
+if [ -z "$PGEN_EXTRA_KNOW_HOST" ]; then echo "set \$PGEN_EXTRA_KNOW_HOST variable." && exit 1; fi
+if [ -z "$PGEN_IMAGE" ]; then echo "set \$PGEN_IMAGE variable." && exit 1; fi
+if [ -z "$PGEN_BRANCH" ]; then echo "set \$PGEN_BRANCH variable." && exit 1; fi
+if [ -z "$PGEN_OUTPUT_FILE" ]; then echo "set \$PGEN_OUTPUT_FILE variable." && exit 1; fi
+
+# Install pipeline generator.
+if ! command -v pipeline-generator &> /dev/null
+then
+    echo "installing pipeline-generator"
+    pip install -U git+https://github.com/Marquesledivan/pipeline-generator.git@v0.0.1
+fi
+
+# Clean cache.
+find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;
+
+pipeline-generator -i $PGEN_IMAGE -e $PGEN_EXTRA_KNOW_HOST -o $PGEN_OUTPUT_FILE -b $PGEN_BRANCH
+
+# Add to commit.
+git add .
